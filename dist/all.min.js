8c60c319c23484ba8bda52d39bb083d21383217a
Anthony Stabile <anthony@decodedigital.co>
2017-06-15 14:10:13 -0500

    fixed a typo

--- dist/all.min.js
+++ dist/all.min.js
@@ -6,7 +6,7 @@ const app = new Vue({
       {title: 'Dress Up', url: 'https://altvr.space/dressup/'},
       {title: 'Video Jukebox', url: 'https://video-jukebox.firebaseapp.com'},
       {title: 'Avatar Interactables', url: 'https://altvr.space/apps/avatarinteractables/'},
-      {title: 'Holo Against Humanity', url: 'http://hah.altvr.com/play'},
+      {title: 'Holo Against Humanity', url: 'http://hah.altvr.com/play'}
     ]
   },
   methods: {
