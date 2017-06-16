1ba6dc4955790dd358dea299ee8cae5cf6c770e2
Anthony Stabile <anthony@decodedigital.co>
2017-06-16 16:19:08 -0500

    changed folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,26 @@ const gulp = require('gulp')
 const concat = require('gulp-concat')
 
 gulp.task('js', () => {
-  return gulp.src(['components/**/*.js'])
-    .pipe(concat('all.min.js', {newline: '\n'}))
+  return gulp.src([
+    'src/js/jquery.js',
+    'src/js/bootstrap.js',
+    'src/js/propeller.js',
+    'src/js/**/*.js'
+  ]).pipe(concat('all.js', {newline: '\n'}))
+    .pipe(gulp.dest('dist'))
+})
+gulp.task('css', () => {
+  return gulp.src([
+    'src/css/bootstrap.css',
+    'src/css/propeller.css',
+    'src/css/**/*.css'
+  ]).pipe(concat('all.css'))
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('default', ['js'], () => {
-  gulp.watch(['components/**/*.js'], ['js'])
+gulp.task('dev', ['js', 'css'], () => {
+  gulp.watch(['src/**/*.js'], ['js'])
+  gulp.watch(['src/**/*.css'], ['css'])
 })
+
+gulp.task('default', ['js', 'css'], () => {})
